0964411f94fb43214a68829efaec60a9a19a837c
Phil Pepin <ppepin34@gmail.com>
2022-05-21 18:28:11 -0400

    create routes for posts

--- models/User.js
+++ models/User.js
@@ -18,6 +18,10 @@ User.init(
             primaryKey: true,
             autoIncrement: true
         },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         email: {
             type: DataTypes.STRING,
             allowNull: false,
